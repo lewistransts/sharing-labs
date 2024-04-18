ddd2eeeae6a17c45b6f67c3e201c8282adac49cb
Khai Tran <khai.tran@saigontechnology.com>
2024-04-18 10:15:59 +0700

    refactor: Remove unused semi-sync replication configurations

--- 02.mysql-semi-sync-replication/init/master/init.sql
+++ 02.mysql-semi-sync-replication/init/master/init.sql
@@ -1,6 +1,3 @@
 CREATE USER 'replica_user'@'%' IDENTIFIED WITH mysql_native_password BY 'rep_password';
 GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
 FLUSH PRIVILEGES;
-
-INSTALL PLUGIN rpl_semi_sync_source SONAME 'semisync_source.so';
-SET GLOBAL rpl_semi_sync_source_enabled = 1;
--- 02.mysql-semi-sync-replication/init/slave1/init.sql
+++
@@ -1,2 +0,0 @@
-INSTALL PLUGIN rpl_semi_sync_replica SONAME 'semisync_replica.so';
-SET GLOBAL rpl_semi_sync_replica_enabled = 1;
